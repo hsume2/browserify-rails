531c6d3b8c3ab46e9856f1d87c47f0bbffff3cbb
Derek Lindahl <dlindahl@customink.com>
2014-03-24 10:50:06 -0700

    Fix intermittent test failure
    * Set the asset cache to nil so that Sprockets doesn't even attempt to
      cache its output
    
    * Sleep for one second to ensure that Sprockets can detect the change in
      the file modification time.
    
      Sprockets uses `File.stat` internally to detect file changes. The
      time resolution of that call is only one second, so any changes that
      occur within the same one second clock cycle cannot be picked up by
      Sprockets and thus it cannot tell that a file changed.
    
      Unfortunately, this means that test runs will always be at least one
      second long. Stubbing out the internals of Sprockets is a possibility,
      but it could be a fragile coupling between this and Sprockets, plus
      it would add a dependency to the gem.

--- test/compilation_test.rb
+++ test/compilation_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class BrowserifyTest < ActionController::IntegrationTest
 
   setup do
-    FileUtils.rm_rf(File.join(Rails.root, 'tmp/cache'))
-    FileUtils.mkdir_p(File.join(Rails.root, 'tmp'))
+    Rails.application.assets.cache = nil
+
     FileUtils.cp(File.join(Rails.root, 'app/assets/javascripts/application.js.example'), File.join(Rails.root, 'app/assets/javascripts/application.js'))
     FileUtils.cp(File.join(Rails.root, 'app/assets/javascripts/foo.js.example'), File.join(Rails.root, 'app/assets/javascripts/foo.js'))
   end
@@ -25,6 +25,9 @@ class BrowserifyTest < ActionController::IntegrationTest
     get "/assets/application.js"
     assert_response :success
     assert @response.body.include?(";(function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require==\"function\"&&require;if(!s&&o)return o(n,!0);if(r)return r(n,!0);throw new Error(\"Cannot find module '\"+n+\"'\")}var u=t[n]={exports:{}};e[n][0].call(u.exports,function(t){var r=e[n][1][t];return i(r?r:t)},u,u.exports)}return t[n].exports}var r=typeof require==\"function\"&&require;for(var s=0;s<n.length;s++)i(n[s]);return i})({1:[function(require,module,exports){\nvar foo = require('./foo');\n\n},{\"./foo\":2}],2:[function(require,module,exports){\nmodule.exports = function (n) { return n * 11 }\n\n},{}]},{},[1])\n"), 'Output did not match'
+
+    # Ensure that Sprockets can detect the change to the file modification time
+    sleep 1
 
     File.open(File.join(Rails.root, 'app/assets/javascripts/foo.js'), 'w+') do |f|
       f.puts "module.exports = function (n) { return n * 12 }"
